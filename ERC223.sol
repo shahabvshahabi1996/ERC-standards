bb060c4a51fc2f68d6277c40bfc40d56208c139f
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-07 17:48:08 +0330

    added isContract method to ERC223

--- ERC223.sol
+++ ERC223.sol
@@ -1,2 +1,44 @@
 pragma solidity ^0.4.0;
 
+
+contract Token {
+    uint256 public totalSupply;
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+contract ERC223 is Token {
+   //fall back function added in ERC223 
+    function() payable {
+        revert();
+    }
+
+    function isContract(address _address) returns(bool) {
+        uint length;
+        assembly {
+            length := extcodesize(_address)
+        }
+        if (length > 0) {
+            return true;
+        }else {
+            return false;
+        }   
+    }
+
+    function transfer(address _to, uint256 value) returns (bool success) {
+        //first we must check that is the _to address is a contract address ?? or not
+
+    }    
+
+    function transfer(address _to, uint256 value, bytes _data) returns (bool success) {
+        //first we must check that is the _to address is a contract address ?? or not
+        
+    }
+
+}
