3904f1dff2c29fe13ca0d0d483b603959586b5e6
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-07 18:03:44 +0330

    added transfer methods to ERC223 and added tokenFallback and deletd some events and function from ERC20

--- ERC223.sol
+++ ERC223.sol
@@ -1,20 +1,23 @@
 pragma solidity ^0.4.0;
 
 
-contract Token {
-    uint256 public totalSupply;
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 value) returns (bool success);
-    function transferFrom(address _from, address _to, uint256 value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+contract TokenReciver {
+    function tokenFallBack(address _from, uint value, bytes _data) {}    
 }
 
 
-contract ERC223 is Token {
-   //fall back function added in ERC223 
+contract Token {
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 value) returns (bool success);
+    function transfer(address _to, uint256 value, bytes _data) returns (bool success);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value, bytes _data);
+}
+
+
+contract ERC223 is Token, TokenReciver {
+   //fall back function added in ERC223
+    mapping (address=>uint) _balanceOf;
+
     function() payable {
         revert();
     }
@@ -32,13 +35,34 @@ contract ERC223 is Token {
     }
 
     function transfer(address _to, uint256 value) returns (bool success) {
-        //first we must check that is the _to address is a contract address ?? or not
-
+        bytes memory empty;
+        if (value > 0 && _balanceOf[msg.sender] > value) {
+            _balanceOf[msg.sender] -= value;
+            _balanceOf[_to] += value;
+            if (isContract(_to)) {
+                TokenReciver reciver = TokenReciver(_to);
+                reciver.tokenFallBack(msg.sender, value, empty);
+            }
+            Transfer(msg.sender, _to, value, empty);
+            return true;
+        }else {
+            return false;
+        }
     }    
 
     function transfer(address _to, uint256 value, bytes _data) returns (bool success) {
-        //first we must check that is the _to address is a contract address ?? or not
-        
+        bytes memory empty;
+        if (value > 0 && _balanceOf[msg.sender] > value) {
+            _balanceOf[msg.sender] -= value;
+            _balanceOf[_to] += value;
+            if (isContract(_to)) {
+                TokenReciver reciver = TokenReciver(_to);
+                reciver.tokenFallBack(msg.sender, value, _data);
+            }
+            Transfer(msg.sender, _to, value, _data);
+            return true;
+        }else {
+            return false;
+        }
     }
-
 }
